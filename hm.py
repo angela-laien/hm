7cf0dda125d1fa93549d0942b0b62d24e261302c
angela-laien <laienxie@gmail.com>
2020-11-06 22:55:18 -0800

    tried to implement sending emails from app

--- hm.py
+++ hm.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import smtplib
 
 app = Flask(__name__)
 
@@ -26,12 +27,19 @@ def form():
     email_address = request.form.get("email_address")
     details = request.form.get("details")
 
+    message = "You message has been submitted"
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login("angelaxie@gmail.com", "PASSWORD")
+    server.sendmail("angelaxie@gmail.com", email_address, message)
+
     if not email_address or not details:
         error_statement = "All Form Fields Required"
         return render_template("contact.html",  
             error_statement = error_statement,
             email_address = email_address,
-            details = details)
+            details = details,
+            message = message)
 
     queries.append(f"{email_address} | {details}")
     return render_template("form.html", queries = queries, title=title)
